453ba163de3e3815c7a1040b72c5d58de11f0e2e
Mariana Gabriely <mariana.silva@atos.net>
2026-04-02 14:32:31 -0300

    ✨ feat: cria api inicial e valida conexao com azure openai

---
+++ backend/app/main.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI
+from langchain_openai import AzureChatOpenAI
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import Field
+
+class Settings(BaseSettings):
+    azure_openai_api_key: str = Field(default="", env="AZURE_OPENAI_API_KEY")
+    azure_openai_endpoint: str = Field(default="", env="AZURE_OPENAI_ENDPOINT")
+    azure_openai_api_version: str = Field(default="", env="AZURE_OPENAI_API_VERSION")
+    azure_openai_chat_deployment: str = Field(default="", env="AZURE_OPENAI_CHAT_DEPLOYMENT")
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        extra="ignore",
+    )
+
+settings = Settings()
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "funcionando!"}
+
+@app.get("/chat")
+def chat():
+    model = AzureChatOpenAI(
+        api_key=settings.azure_openai_api_key,
+        azure_endpoint=settings.azure_openai_endpoint,
+        api_version=settings.azure_openai_api_version,
+        azure_deployment=settings.azure_openai_chat_deployment,
+        temperature=0,
+    )
+
+    response = model.invoke("O que é RAG?")
+    return {"answer": response.content}
